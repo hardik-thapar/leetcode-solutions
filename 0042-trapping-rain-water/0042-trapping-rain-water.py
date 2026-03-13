ff7b7db07bac522f2fb7a8da5bb05fe35c9cc2a5
Hardik Thapar <hardikthapar1@gmail.com>
2026-03-13 14:58:28 +0530

    Time: 12 ms (34.22%), Space: 20.9 MB (95.42%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,16 +1,15 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        area=0
-        i=0
-        j=len(height)-1
         ml=0
         mr=0
-        
+        i=0
+        j=len(height)-1
+        area=0
         while(i<j):
             ml=max(ml,height[i])
             mr=max(mr,height[j])
-            area=area+ml-height[i]
-            area=area+mr-height[j]
+            area+=ml-height[i]
+            area+=mr-height[j]
             if(height[i]<height[j]):
                 i+=1
             else:
