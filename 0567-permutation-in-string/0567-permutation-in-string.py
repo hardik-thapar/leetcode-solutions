0d1903a75c21059d68dd6d759f786fd8c08d9360
Hardik Thapar <hardikthapar1@gmail.com>
2026-03-15 13:08:32 +0530

    Time: 27 ms (40.01%), Space: 19.3 MB (80.04%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -7,10 +7,10 @@ class Solution:
         for r in range(len(s2)):
             f2[s2[r]]=f2.get(s2[r],0)+1
             while(r-l+1 > k):
-                if s2[l] in f2:
-                    f2[s2[l]]-=1
-                    if f2[s2[l]]<=0:
-                        del f2[s2[l]]
+                
+                f2[s2[l]]-=1
+                if f2[s2[l]]<=0:
+                    del f2[s2[l]]
                 l+=1
             if f1==f2:
                 return True
