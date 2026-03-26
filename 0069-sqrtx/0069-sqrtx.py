7b98d226eebae1cd96cbf6e82ebda0d85829a36f
Hardik Thapar <hardikthapar1@gmail.com>
2026-03-26 15:21:58 +0530

    Time: 0 ms (100%), Space: 19.2 MB (94.38%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.py
+++ 0069-sqrtx/0069-sqrtx.py
@@ -4,11 +4,10 @@ class Solution:
         end=x
         m=0
         while(st<=end):
-            mid=int((st+end)/2)
-            if mid*mid<=x:
+            mid = int((st+end)/2)
+            if mid*mid <= x:
                 st=mid+1
                 m=mid
             else:
                 end=mid-1
-        return m  
-       
+        return m
