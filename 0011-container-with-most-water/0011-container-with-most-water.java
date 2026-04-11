fee70d122c7aea9036991753a9d5d2d0bc3e519c
Hardik Thapar <hardikthapar1@gmail.com>
2026-04-11 17:02:40 +0530

    Time: 5 ms (83.19%), Space: 77.5 MB (36.03%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -3,8 +3,8 @@ class Solution {
         int st = 0;
         int end = height.length - 1;
         int area = 0;
-        int left=0;
-        int right=0;
+        // int left=0;
+        // int right=0;
         while(st<end){
             int a = Math.min(height[st],height[end])*(end-st);
             if(height[st]<height[end]){
