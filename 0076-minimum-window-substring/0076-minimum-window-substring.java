5eed65c0e7274006f84cd7899db36c92e79db8fa
Hardik Thapar <hardikthapar1@gmail.com>
2026-04-13 14:42:42 +0530

    Time: 20 ms (38.62%), Space: 47.7 MB (26.28%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,40 +1,46 @@
 class Solution {
     public String minWindow(String s, String t) {
-       HashMap<Character, Integer> tmap = new HashMap<>();
-       for (char c: t.toCharArray()){
-        tmap.put(c, tmap.getOrDefault(c,0)+1);
-       } 
-       int need = tmap.size();
-       int start = 0;
-       int st=0;
-       int minLen = Integer.MAX_VALUE;
-       int have = 0;
-
-       HashMap<Character, Integer> smap = new HashMap<>();
-       for(int end = 0; end < s.length(); end++){
-        char c = s.charAt(end);
-        smap.put(c,smap.getOrDefault(c,0)+1);
-        if (tmap.containsKey(c) && smap.get(c).equals(tmap.get(c))){
-            have++;
+        // a map for t freq
+        HashMap<Character, Integer> tmap = new HashMap<>();
+        for(char c : t.toCharArray()){
+            tmap.put(c,tmap.getOrDefault(c,0)+1);
         }
-        while(have == need){
-            if((end-st+1)<minLen){
-                start = st;
-                minLen = end-st+1;
-            }
-            char left = s.charAt(st);
-            smap.put(left,smap.get(left)-1);
-            if (tmap.containsKey(left) && smap.get(left) < tmap.get(left)){
-                have--;
 
-            }
-            st++;
-        }
         
-       }
-        if (minLen == Integer.MAX_VALUE) return "";
-        else{
-            return s.substring(start,start+minLen);
+        int st=0;
+        int have = 0;
+        int need = tmap.size();
+        int min_len = Integer.MAX_VALUE;
+        int start = 0;
+        // keeping a track of window with map 
+        HashMap<Character, Integer> smap = new HashMap<>();
+        for(int end = 0; end < s.length(); end++){
+            char c = s.charAt(end);
+            smap.put(c,smap.getOrDefault(c,0)+1);
+            if(tmap.containsKey(c) && tmap.get(c).equals(smap.get(c))){
+                have++;
+            }
+            while(have == need){
+                // min_len = Math.min(min_len,end-st+1);
+                char x = s.charAt(st);
+                if((end-st+1) < min_len){
+                    min_len = end-st+1;
+                    start = st;
+                }
+                smap.put(x,smap.get(x)-1);
+                // if(smap.get(s.charAt(st)) <= 0){
+                //     smap.remove(s.charAt(st));
+                // }
+                
+                if(tmap.containsKey(x) && (smap.get(x) < tmap.get(x))){
+                    have--;
+                }
+
+            st++;
+            }
         }
+        if(min_len == Integer.MAX_VALUE) {return ""; }
+        else{
+        return s.substring(start,start+min_len); }
     }
 }
