0cb23fa4d8dd6e666522f20445f342110f6fecfb
Hardik Thapar <hardikthapar1@gmail.com>
2026-04-15 09:57:22 +0530

    Time: 2 ms (87.56%), Space: 49.2 MB (76.98%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.java
+++ 0143-reorder-list/0143-reorder-list.java
@@ -10,17 +10,14 @@
  */
 class Solution {
     public void reorderList(ListNode head) {
-        if(head==null || head.next==null) return;
         ListNode slow = head;
         ListNode fast = head;
         while(fast!=null && fast.next!=null){
             fast = fast.next.next;
-            slow=slow.next;
+            slow = slow.next;
         }
-        // head of second list is at slow
-        // we have to reverse it
-        ListNode prev = null;
         ListNode curr = slow.next;
+        ListNode prev = null;
         while(curr!=null){
             ListNode next = curr.next;
             curr.next = prev;
@@ -28,40 +25,20 @@ class Solution {
             curr = next;
         }
         slow.next = null;
-        // new reversed head at prev
-        // we have two lists, first list head is at head and secodn list head at prev;
+
         ListNode first = head;
         ListNode second = prev;
-        int count = 0;
-        ListNode newHead = new ListNode(0);
-        ListNode track = newHead;
-
-        while(first!=null && second!=null){
-            if(count%2==0){
-                track.next = first;
-                first = first.next;
-            }
-            else{
-                track.next = second;
-                second = second.next;
-            }
-            track = track.next;
-            count++;
-        }
-
-        while(first!=null){
-            track.next = first;
-            first=first.next;
-            track = track.next;
-        }
-
         while(second!=null){
-            track.next = second;
-            second = second.next;
-            track = track.next;
+            ListNode t1 = first.next;
+            ListNode t2 = second.next;
+            first.next = second;
+            second.next = t1;
+            
+            first = t1;
+            second = t2;
+
         }
 
-        head = newHead.next;
-
+        return;
     }
 }
