168c1aad622db785ada8d8fa262400e1ed123b9f
Hardik Thapar <hardikthapar1@gmail.com>
2026-05-05 19:43:31 +0530

    Time: 1 ms (28.73%), Space: 45.1 MB (98.28%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.java
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -16,20 +16,14 @@
 class Solution {
     public int height(TreeNode root){
         if(root==null) return 0;
-        return Math.max(height(root.left),height(root.right))+1;
-    }
-    public boolean hb(TreeNode root){
-       if(root==null) return true;
-       boolean l = hb(root.left);
-       boolean r = hb(root.right);
-       int hl = height(root.left);
-       int hr = height(root.right);
-       if(l && r && Math.abs(hl-hr)<=1) return true;
-       return false;
-
+        int l = height(root.left);
+        int r = height(root.right);
+        if(l==-1 || r==-1) return -1;
+        if(Math.abs(l-r)>1) return -1;
+        return Math.max(l,r)+1;
     }
     public boolean isBalanced(TreeNode root) {
-        if(root==null) return true;
-        return hb(root);
+        return height(root)!=-1;
+
     }
 }
