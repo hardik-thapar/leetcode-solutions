6da23904e73cad19fc1515f3f47633ef72691832
Hardik Thapar <hardikthapar1@gmail.com>
2026-05-16 16:51:23 +0530

    Time: 3 ms (67.37%), Space: 46.4 MB (52.04%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.java
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.java
@@ -14,31 +14,20 @@
  * }
  */
 class Solution {
-    public int check(TreeNode root, TreeNode subRoot){
-        if(root==null && subRoot==null) return 1;
-        if(root==null) return 0;
-        if(subRoot==null && root!=null) return 0;
-        if(root==null && subRoot!=null) return 0;
-        if(root.val!=subRoot.val) return 0;
-        // if(check(root.left, subRoot.left) && check(root.right, subRoot.right)) return true;
-        int l = check(root.left, subRoot.left);
-        int r = check(root.right, subRoot.right);
-        if(l==1 && r==1) return 1;
-        // int left = check(root.left, subRoot);
-        // int right = check(root.right,subRoot);
-        // // return (check(root.left, subRoot)==1 || check(root.right,subRoot)==1);
-        // if(left==1 || right==1) return 1;
-        return 0;
-
-
+    public boolean check(TreeNode root, TreeNode subRoot){
+        if(root==null && subRoot==null) return true;
+        if(root==null && subRoot!=null) return false;
+        if(root!=null && subRoot==null) return false;
+        if(root.val!=subRoot.val) return false;
+        return (check(root.left,subRoot.left) && check(root.right, subRoot.right));
     }
-    
     public boolean isSubtree(TreeNode root, TreeNode subRoot) {
         if(root==null && subRoot==null) return true;
         if(root==null && subRoot!=null) return false;
-        if(root!=null && subRoot==null) return true; 
-        if(root==null) return false;
-        if(check(root,subRoot)==1) return true;
-        return(isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot));
+        if(root!=null && subRoot==null) return false;
+        if(root.val==subRoot.val){
+            if(check(root,subRoot)) return true;
+        }
+        return(isSubtree(root.left,subRoot) || isSubtree(root.right,subRoot));
     }
 }
