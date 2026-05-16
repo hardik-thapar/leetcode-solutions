56dfaebac183102a7076eb2da8031d927f6c1e7d
Hardik Thapar <hardikthapar1@gmail.com>
2026-05-16 17:17:03 +0530

    Time: 6 ms (97.41%), Space: 47.9 MB (16.46%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.java
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.java
@@ -9,24 +9,18 @@
  */
 
 class Solution {
-    public boolean get_path(TreeNode root, TreeNode target, List<TreeNode> list){
-        if(root==null) return false;
-        list.add(root);
-        if(root==target) return true;
-        if(get_path(root.left, target, list) || get_path(root.right, target, list)) return true;
-        else list.remove(root);
-        return false;
-    }
-
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        List<TreeNode> l1 = new ArrayList<>();
-        List<TreeNode> l2 = new ArrayList<>();
-        get_path(root,p,l1);
-        get_path(root,q,l2);
-        int n = Math.min(l1.size(),l2.size());
-        for(int i=1; i<n; i++){
-            if(!l1.get(i).equals(l2.get(i))) return l1.get(i-1);
+        while(root!=null){
+            if(p.val<root.val && q.val<root.val){
+                root=root.left;
+            }
+            else if(p.val>root.val && q.val>root.val){
+                root=root.right;
+            }
+            else{
+                return root;
+            }
         }
-        return l1.get(n-1);
+        return root;
     }
 }
