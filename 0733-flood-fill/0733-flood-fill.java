5812bd77efedba3c13be42e4ec5ae52824423d09
Hardik Thapar <hardikthapar1@gmail.com>
2026-06-20 16:25:36 +0530

    Time: 0 ms (100%), Space: 47 MB (24.93%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,20 +1,17 @@
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        int r = image.length;
-        int c = image[0].length;
-        if(sr>r || sc>c) return image;
         int og = image[sr][sc];
-        return dfs(image,sr,sc,color,og);
-    }
-    public int[][] dfs(int[][] image, int sr, int sc, int color, int og){
-        if(sr >= 0 && sr < image.length && sc >= 0 && sc < image[0].length && image[sr][sc] == og){
-        if(image[sr][sc]!=color){
-            image[sr][sc] = color;
-            dfs(image,sr-1,sc,color,og);
-            dfs(image,sr+1,sc,color,og);
-            dfs(image,sr,sc-1,color,og);
-            dfs(image,sr,sc+1,color,og);
-        }}
+        dfs(image, sr, sc, color, og);
         return image;
+    }
+
+    void dfs(int[][] image, int sr, int sc, int color, int og){
+        if(image[sr][sc]==color) return;
+        image[sr][sc] = color;
+        if(sr>0 && image[sr-1][sc]==og) dfs(image, sr-1, sc, color, og);
+        if(sr<image.length-1 && image[sr+1][sc]==og) dfs(image, sr+1, sc, color, og);
+        if(sc>0 && image[sr][sc-1]==og) dfs(image, sr, sc-1, color, og);
+        if(sc<image[0].length-1 && image[sr][sc+1]==og) dfs(image, sr, sc+1, color, og);
+        return;
     }
 }
