f4b9f1ae2feb8b30acb8f55347b62f204255f67a
Hardik Thapar <hardikthapar1@gmail.com>
2026-06-20 17:59:06 +0530

    Time: 2 ms (99.85%), Space: 52.4 MB (33.14%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -5,24 +5,21 @@ class Solution {
         int count=0;
         for(int i=0;i<r;i++){
             for(int j=0;j<c;j++){
-                if(grid[i][j] == '1'){
-                    count++;
+                if(grid[i][j]=='1'){
                     dfs(grid,i,j);
+                    count++;
                 }
             }
         }
         return count;
     }
-    public void dfs(char[][] grid, int i, int j){
-        if(i < grid.length && i>=0 && j < grid[0].length && j>=0){
-            if(grid[i][j]=='1'){
-                grid[i][j]='0';
-                dfs(grid,i-1,j);
-                dfs(grid,i+1,j);
-                dfs(grid,i,j-1);
-                dfs(grid,i,j+1);
-            }
-        }
+    void dfs(char[][] grid, int i, int j){
+        if(grid[i][j]=='0') return;
+        grid[i][j] = '0';
+        if(i>0) dfs(grid, i-1, j);
+        if(i<grid.length-1) dfs(grid, i+1, j);
+        if(j>0) dfs(grid, i, j-1);
+        if(j<grid[0].length-1) dfs(grid, i, j+1);
         return;
     }
 }
