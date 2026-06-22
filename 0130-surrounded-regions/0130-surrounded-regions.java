63d50cd095ba63d422266b88bb02c64900f51825
Hardik Thapar <hardikthapar1@gmail.com>
2026-06-22 15:16:17 +0530

    Time: 2 ms (73.22%), Space: 47.9 MB (21.92%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.java
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -4,20 +4,18 @@ class Solution {
         
         int r = board.length;
         int c = board[0].length;
-        boolean[][] mark = new boolean[r][c];
 
         for(int i=0;i<r;i++){
             for(int j=0;j<c;j++){
                 if((i==0 || i==r-1 || j==0 || j==c-1) && board[i][j]=='O'){
-                    // mark[i][j] = true;
-                    dfs(board, i, j, mark);
+                    dfs(board, i, j);
                 }
             }
         }
 
         for(int i=0;i<r;i++){
             for(int j=0;j<c;j++){
-                if(mark[i][j]){
+                if(board[i][j]=='T'){
                     board[i][j] = 'O';
                 }
                 else board[i][j]='X';
@@ -27,13 +25,13 @@ class Solution {
         }
     
 
-    public void dfs(char[][] board, int i, int j, boolean[][] mark){
-        if(mark[i][j]) return;
-        mark[i][j] = true;
-        if(i>0 && board[i-1][j]=='O') dfs(board,i-1,j,mark);
-        if(i<board.length-1 && board[i+1][j]=='O') dfs(board,i+1,j,mark);
-        if(j>0 && board[i][j-1]=='O') dfs(board,i,j-1,mark);
-        if(j<board[0].length-1 && board[i][j+1]=='O') dfs(board,i,j+1,mark);
+    public void dfs(char[][] board, int i, int j){
+        if(board[i][j]=='T') return;
+        board[i][j] = 'T';
+        if(i>0 && board[i-1][j]=='O') dfs(board,i-1,j);
+        if(i<board.length-1 && board[i+1][j]=='O') dfs(board,i+1,j);
+        if(j>0 && board[i][j-1]=='O') dfs(board,i,j-1);
+        if(j<board[0].length-1 && board[i][j+1]=='O') dfs(board,i,j+1);
         return;
     }
 }
