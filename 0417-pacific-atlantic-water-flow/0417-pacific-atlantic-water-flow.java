3511869321fbd27a038dcc76114aa2a3723fef8e
Hardik Thapar <hardikthapar1@gmail.com>
2026-06-22 16:05:42 +0530

    Time: 4 ms (98.6%), Space: 47.3 MB (83.18%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.java
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.java
@@ -1,46 +1,24 @@
 class Solution {
-
-    public void dfs(int[][] heights, int i, int j, boolean[][] mark){
-        if(mark[i][j]) return;
-        mark[i][j] = true;
-        // left
-        if(i-1>=0 && heights[i-1][j]>=heights[i][j]) dfs(heights,i-1,j,mark);
-        // right
-        if(i+1<heights.length && heights[i+1][j]>=heights[i][j]) dfs(heights,i+1,j,mark);
-        // up
-        if(j-1>=0 && heights[i][j-1] >= heights[i][j]) dfs(heights,i,j-1,mark);
-        // down
-        if(j+1<heights[0].length && heights[i][j+1] >= heights[i][j]) dfs(heights,i,j+1,mark);
-        return;
-    }
-
     public List<List<Integer>> pacificAtlantic(int[][] heights) {
         List<List<Integer>> res = new ArrayList<>();
         List<Integer> temp = new ArrayList<>();
         int r = heights.length;
         int c = heights[0].length;
-
         boolean[][] pacific = new boolean[r][c];
         boolean[][] atlantic = new boolean[r][c];
-        
-        // pacific 
+        // pacific loop
         for(int i=0;i<r;i++){
             for(int j=0;j<c;j++){
-                if(i==0 || j==0){
-                    dfs(heights,i,j,pacific);
-                }
+                if(i==0 || j==0) dfs(heights,i,j,pacific);
             }
         }
-
-        // atlantic 
+        // atlantic loop
         for(int i=0;i<r;i++){
             for(int j=0;j<c;j++){
-                if(i==r-1 || j==c-1){
-                    dfs(heights,i,j,atlantic);
-                }
+                if(i==r-1 || j==c-1) dfs(heights,i,j,atlantic);
             }
         }
-
+        // list loop
         for(int i=0;i<r;i++){
             for(int j=0;j<c;j++){
                 if(pacific[i][j] && atlantic[i][j]){
@@ -51,6 +29,15 @@ class Solution {
                 }
             }
         }
-    return res;
+        return res;
     }
-}
+        public void dfs(int[][] heights, int i, int j, boolean[][] mark){
+            if(mark[i][j]) return;
+            mark[i][j] = true;
+            if(i>0 && heights[i-1][j]>=heights[i][j]) dfs(heights,i-1,j,mark);
+            if(i<heights.length-1 && heights[i+1][j]>=heights[i][j]) dfs(heights,i+1,j,mark);
+            if(j>0 && heights[i][j-1]>=heights[i][j]) dfs(heights,i,j-1,mark);
+            if(j<heights[0].length-1 && heights[i][j+1]>=heights[i][j]) dfs(heights,i,j+1,mark);
+            return;
+        }
+    }
