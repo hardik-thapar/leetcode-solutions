fd1619414840a6d909a27eca54d33caec37ea4d2
Hardik Thapar <hardikthapar1@gmail.com>
2026-06-24 14:18:26 +0530

    Time: 13 ms (91.72%), Space: 48.6 MB (86.72%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.java
+++ 0542-01-matrix/0542-01-matrix.java
@@ -1,44 +1,48 @@
 class Solution {
     public int[][] updateMatrix(int[][] mat) {
-        int r = mat.length;
-        int c = mat[0].length; 
+        int m = mat.length;
+        int n = mat[0].length;
         Queue<int[]> q = new LinkedList<>();
-        
-        for(int i=0;i<r;i++){
-            for(int j=0;j<c;j++){
-                if(mat[i][j]==0){
-                    q.add(new int[]{i,j});
+
+        for(int a=0;a<m;a++){
+            for(int b=0;b<n;b++){
+                if(mat[a][b]==0){
+                    q.add(new int[]{a,b});
                 }
-                else {mat[i][j]=-1;}
+                if(mat[a][b]==1) mat[a][b]=-1;
             }
         }
-        
+        int level = 1;
         while(!q.isEmpty()){
             int size = q.size();
             for(int x=0;x<size;x++){
                 int[] arr = q.poll();
                 int i = arr[0];
                 int j = arr[1];
-                int as = mat[i][j];
+                // up
                 if(i>0 && mat[i-1][j]==-1){
-                    mat[i-1][j]=as+1;
+                    mat[i-1][j]=level;
                     q.add(new int[]{i-1,j});
-                    }
-                if(i<r-1 && mat[i+1][j]==-1){
-                    mat[i+1][j] = as+1;
+                }
+                // down
+                if(i<m-1 && mat[i+1][j]==-1){
+                    mat[i+1][j]=level;
                     q.add(new int[]{i+1,j});
-                    }
+                }
+                // left
                 if(j>0 && mat[i][j-1]==-1){
-                    mat[i][j-1] = as+1;
+                    mat[i][j-1]=level;
                     q.add(new int[]{i,j-1});
                 }
-                if(j<c-1 && mat[i][j+1]==-1){
-                    mat[i][j+1] = as+1;
-                    q.add(new int[]{i,j+1}); 
+                // right
+                if(j<n-1 && mat[i][j+1]==-1){
+                    mat[i][j+1]=level;
+                    q.add(new int[]{i,j+1});
                 }
             }
-            // level++;
+            level++;
         }
         return mat;
+        }
+        
     }
-}
