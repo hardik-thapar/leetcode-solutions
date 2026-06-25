7366c83b6e42e6cd404634c5de4a1b7f01504879
Hardik Thapar <hardikthapar1@gmail.com>
2026-06-25 20:02:45 +0530

    Time: 2 ms (90.7%), Space: 45.2 MB (5.18%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -1,58 +1,40 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
-        int r = grid.length;
-        int c = grid[0].length;
-        // boolean
+        int m = grid.length;
+        int n = grid[0].length;
+        int fresh = 0;
+        int converted = 0;
         int level = -1;
         Queue<int[]> q = new LinkedList<>();
-        int res = 0;
-        for(int i=0;i<r;i++){
-            for(int j=0;j<c;j++){
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
                 if(grid[i][j]==2) q.add(new int[]{i,j});
-                if(grid[i][j]==1) res++;
+                if(grid[i][j]==1){
+                    grid[i][j]=-1;
+                    fresh++;}
             }
         }
-        if(res==0) return 0;
-        if(q.isEmpty()) return -1;
-        
+        if(fresh==0) return 0;
         while(!q.isEmpty()){
-            
             int size = q.size();
-           
             for(int x=0;x<size;x++){
-                int[] arr = q.poll();
-                int i = arr[0];
-                int j = arr[1];
-                // up
-                if(i>0 && grid[i-1][j]==1){
-                    grid[i-1][j]=2;
-                    res--;
-                    q.add(new int[]{i-1,j});
+            int[] node = q.poll();
+            int i = node[0];
+            int j = node[1];
+            int[][] dir = {{-1,0},{1,0},{0,-1},{0,1}};
+            for(int[] d: dir){
+                int nr = i+d[0];
+                int nc = j+d[1];
+                if(nr>=0 && nr<m && nc>=0 && nc<n && grid[nr][nc]==-1){
+                    converted++;
+                    grid[nr][nc] = 2;
+                    q.add(new int[]{nr,nc});
                 }
-                // down
-                if(i<r-1 && grid[i+1][j]==1){
-                    grid[i+1][j]=2;
-                    res--;
-                    q.add(new int[]{i+1,j});
-                }
-                // left
-                if(j>0 && grid[i][j-1]==1){
-                    grid[i][j-1]=2;
-                    res--;
-                    q.add(new int[]{i,j-1});
-                }
-                // right
-                if(j<c-1 && grid[i][j+1]==1){
-                    grid[i][j+1]=2;
-                    res--;
-                    q.add(new int[]{i,j+1});
-                }
-
-                // mark[i][j] = true;
             }
-            level++;
         }
-        if(res==0) return level;
+        level++;
+        }
+        if(converted==fresh) return level;
         return -1;
     }
 }
