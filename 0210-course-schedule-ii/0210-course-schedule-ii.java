d4ef79c3752a442e296f0f44430d608fd280246d
Hardik Thapar <hardikthapar1@gmail.com>
2026-06-25 10:44:27 +0530

    Time: 4 ms (94.93%), Space: 46.5 MB (90.57%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        int processed = 0;
+        int n = numCourses;
+        int[] indegree = new int[n];
+        int[] res = new int[n];
+        int x = 0;
+        Queue<Integer> q = new LinkedList<>();
+        ArrayList<Integer>[] adj = new ArrayList[n];
+        for(int i=0;i<n;i++){
+            adj[i] = new ArrayList<>();
+        }
+        for(int[] edge: prerequisites){
+            int u = edge[0];
+            int v = edge[1];
+            adj[v].add(u);
+            indegree[u]++;
+        }
+        for(int i=0;i<n;i++){
+            if(indegree[i]==0) q.offer(i);
+        }
+
+        while(!q.isEmpty()){
+            int node = q.poll();
+            res[x] = node;
+            x++;
+            for(int nbr: adj[node]){
+                indegree[nbr]--;
+                if(indegree[nbr]==0) q.offer(nbr);
+            }
+        }
+    if(x==n) return res;
+    return new int[0];
+    }
+}
