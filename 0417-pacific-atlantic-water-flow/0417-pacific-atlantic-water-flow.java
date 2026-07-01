97b681677ffa90576993db5663fc752e28894294
Hardik Thapar <hardikthapar1@gmail.com>
2026-07-01 10:32:38 +0530

    Time: 5 ms (56.23%), Space: 47.5 MB (42.13%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.java
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.java
@@ -1,27 +1,37 @@
 class Solution {
+    private final int[] dirs = {-1,0,1,0,-1};
+    private void dfs(int[][] heights, int[][] ocean, int i, int j){
+        ocean[i][j] = 1;
+        for(int k=0; k<4; k++){
+            int nr = i+dirs[k];
+            int nc = j+dirs[k+1];
+            if(nr>=0 && nr<heights.length && nc>=0 && nc<heights[0].length && heights[nr][nc]>=heights[i][j] && ocean[nr][nc]!=1){
+                dfs(heights, ocean, nr, nc);
+            }
+        }
+    }
     public List<List<Integer>> pacificAtlantic(int[][] heights) {
         List<List<Integer>> res = new ArrayList<>();
         List<Integer> temp = new ArrayList<>();
-        int r = heights.length;
-        int c = heights[0].length;
-        boolean[][] pacific = new boolean[r][c];
-        boolean[][] atlantic = new boolean[r][c];
-        // pacific loop
-        for(int i=0;i<r;i++){
-            for(int j=0;j<c;j++){
-                if(i==0 || j==0) dfs(heights,i,j,pacific);
+        int m = heights.length;
+        int n = heights[0].length;
+        int[][] pacific = new int[m][n];
+        int[][] atlantic = new int[m][n];
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(i==0 || j==0) dfs(heights, pacific, i, j);
             }
         }
-        // atlantic loop
-        for(int i=0;i<r;i++){
-            for(int j=0;j<c;j++){
-                if(i==r-1 || j==c-1) dfs(heights,i,j,atlantic);
+
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(i==m-1 || j==n-1) dfs(heights, atlantic, i, j);
             }
         }
-        // list loop
-        for(int i=0;i<r;i++){
-            for(int j=0;j<c;j++){
-                if(pacific[i][j] && atlantic[i][j]){
+
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(pacific[i][j]==1 && atlantic[i][j]==1){
                     temp = new ArrayList<>();
                     temp.add(i);
                     temp.add(j);
@@ -31,13 +41,4 @@ class Solution {
         }
         return res;
     }
-        public void dfs(int[][] heights, int i, int j, boolean[][] mark){
-            if(mark[i][j]) return;
-            mark[i][j] = true;
-            if(i>0 && heights[i-1][j]>=heights[i][j]) dfs(heights,i-1,j,mark);
-            if(i<heights.length-1 && heights[i+1][j]>=heights[i][j]) dfs(heights,i+1,j,mark);
-            if(j>0 && heights[i][j-1]>=heights[i][j]) dfs(heights,i,j-1,mark);
-            if(j<heights[0].length-1 && heights[i][j+1]>=heights[i][j]) dfs(heights,i,j+1,mark);
-            return;
-        }
-    }
+}
