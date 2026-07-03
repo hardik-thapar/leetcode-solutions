6a77bf6322802f251c7456315ec184acbe8b3e6c
Hardik Thapar <hardikthapar1@gmail.com>
2026-07-03 19:18:19 +0530

    Time: 1 ms (91.19%), Space: 45.2 MB (39.21%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.java
+++ 0684-redundant-connection/0684-redundant-connection.java
@@ -1,20 +1,22 @@
 class Solution {
     private int find(int node, int[] parent){
-        if(parent[node] == node) return node;
-        return find(parent[node], parent);
+        if(parent[node]==node) return node;
+        return parent[node] = find(parent[node], parent);
     }
     public int[] findRedundantConnection(int[][] edges) {
-        int[] parent = new int[edges.length+1];
-        for(int i=1; i<edges.length+1; i++){
+        int n = edges.length+1;
+        int[] parent = new int[n];
+        for(int i=1; i<n; i++){
             parent[i] = i;
         }
+
         for(int[] edge: edges){
             int u = edge[0];
             int v = edge[1];
             int leader1 = find(u, parent);
             int leader2 = find(v, parent);
             if(leader1==leader2) return new int[]{u,v};
-            else parent[leader2] = leader1;
+            parent[leader2] = leader1;
         }
         return new int[]{-1,-1};
     }
