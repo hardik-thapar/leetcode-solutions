26fdc25ee64fb1640481d644642dec5679a9859f
Hardik Thapar <hardikthapar1@gmail.com>
2026-07-03 19:29:18 +0530

    Time: 4 ms (94.77%), Space: 46.9 MB (40.79%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.java
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -1,36 +1,37 @@
 class Solution {
     public int[] findOrder(int numCourses, int[][] prerequisites) {
-        int[] res = new int[numCourses];
-        int ind = 0;
-        int n = 0;
-        List<Integer>[] adj = new ArrayList[numCourses+1];
-        int[] indegree = new int[numCourses+1];
+        int n = numCourses;
+        int[] indegree = new int[n];
+        int[] res = new int[n];
+        int idx = 0;
+        List<Integer>[] adj = new ArrayList[n];
         Queue<Integer> q = new LinkedList<>();
-        for(int i=0;i<numCourses; i++){
+        for(int i=0; i<n; i++){
             adj[i] = new ArrayList<>();
         }
         for(int[] edge: prerequisites){
             int u = edge[0];
             int v = edge[1];
-            indegree[u]++;
             adj[v].add(u);
+            indegree[u]++;
         }
-
-        for(int i=0;i<numCourses; i++){
-            if(indegree[i]==0) q.offer(i);
-        }
-
-        while(!q.isEmpty()){
-            int node = q.poll();
-            res[ind++] = node;
-            n++;
-            for(int nbr: adj[node]){
-                indegree[nbr]--;
-                if(indegree[nbr]==0){
-                    q.offer(nbr);}
+        for(int i=0; i<n; i++){
+            if(indegree[i]==0){
+                q.offer(i);
             }
         }
-        if(n==numCourses) return res;
-        return new int[]{};
+        while(!q.isEmpty()){
+            int node = q.poll();
+            res[idx++] = node;
+            for(int nbr: adj[node]){
+                indegree[nbr]--;
+                if(indegree[nbr]==0) q.offer(nbr);
+            }
+        }
+
+        if(idx==n) return res;
+        return new int[0];
+
+
     }
 }
