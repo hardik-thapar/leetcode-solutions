636ed646ad67e800dd161718c70515d77910bafb
Hardik Thapar <hardikthapar1@gmail.com>
2026-07-03 15:54:08 +0530

    Time: 102 ms (36.65%), Space: 48.5 MB (74.67%) - LeetHub

--- 0752-open-the-lock/0752-open-the-lock.java
+++ 0752-open-the-lock/0752-open-the-lock.java
@@ -1,47 +1,47 @@
 class Solution {
     public int openLock(String[] deadends, String target) {
-        HashSet<String> dead_end = new HashSet<>();
-        HashSet<String> visited = new HashSet<>();
-        for(String word: deadends){
-            dead_end.add(word); 
-        }
         if(target.equals("0000")) return 0;
-        if(dead_end.contains("0000")) return -1;
+        Set<String> dead_end = new HashSet<>();
+        Set<String> visited = new HashSet<>();
         Queue<String> q = new LinkedList<>();
+        for(String dead: deadends){
+            dead_end.add(dead);
+        }
+        if(dead_end.contains("0000")) return -1;
+        
         q.add("0000");
         int level = 0;
+        String next;
         while(!q.isEmpty()){
             int size = q.size();
             level++;
             for(int x=0; x<size; x++){
-                char[] curr = q.poll().toCharArray();
-                
-                for(int pos=0; pos<4; pos++){
-                    char ch = curr[pos];
-                    // -1
-                    if(curr[pos]=='0') curr[pos] = '9';
-                    else curr[pos]-=1;
-                    String next = new String(curr);
-                    if (next.equals(target)) return level;
-                    if(!dead_end.contains(next) && !visited.contains(next)){
-                        q.add(next);
-                    }
-                    visited.add(next);
+                String curr = q.poll();
+                char[] c = curr.toCharArray();
 
-                    curr[pos] = ch;
+                for(int a=0; a<4; a++){
+                    char ch = c[a];
                     // +1
-                    if(curr[pos]=='9') curr[pos] = '0';
-                    else curr[pos]+=1;
-                    next = new String(curr);
+                    if(c[a]=='9') c[a] = '0';
+                    else c[a]+=1;
+                    next = new String(c);
                     if (next.equals(target)) return level;
-                    if(!dead_end.contains(next) && !visited.contains(next)){
-                        q.add(next);
-                    }
+                    if(!dead_end.contains(next) && !visited.contains(next)) q.offer(next);
                     visited.add(next);
-                    curr[pos] = ch;
+                    // -1
+                    c[a] = ch;
+                    if(ch=='0') c[a] = '9';
+                    else c[a]-=1;
+                    next = new String(c);
+                    if (next.equals(target)) return level;
+                    if(!dead_end.contains(next) && !visited.contains(next)) q.offer(next);
+                    c[a] = ch;
+                    visited.add(next);
+                    
                 }
+
             }
-            
+            // level++;
         }
         return -1;
     }
