b821f8caadb39a3b48fd0cd40e5f6512bad5f200
Hardik Thapar <hardikthapar1@gmail.com>
2026-07-06 11:39:50 +0530

    Time: 53 ms (36.39%), Space: 48.2 MB (7.45%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.java
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.java
@@ -10,34 +10,34 @@ class Solution {
         }
     }
     public int minimumEffortPath(int[][] heights) {
-          int m = heights.length;
-          int n = heights[0].length;
-          int[][] cost = new int[m][n];
-          for(int i=0; i<m; i++){
+        int m = heights.length;
+        int n = heights[0].length;
+        int[][] dist = new int[m][n];
+        for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
-                cost[i][j] = Integer.MAX_VALUE;
+                dist[i][j] = Integer.MAX_VALUE;
             }
-          }
-          PriorityQueue<Pair> pq = new PriorityQueue<>((a,b)->Integer.compare(a.value,b.value));
-          cost[0][0] = 0;
-          pq.offer(new Pair(0,0,0));
-          int[] dirs = {-1,0,1,0,-1};
-          while(!pq.isEmpty()){
+        }
+
+        PriorityQueue<Pair> pq = new PriorityQueue<>((a,b)->Integer.compare(a.value,b.value));
+        dist[0][0] = 0;
+        pq.offer(new Pair(0,0,0));
+        int[] dirs = {-1,0,1,0,-1};
+        while(!pq.isEmpty()){
             Pair curr = pq.poll();
-            if(curr.value > cost[curr.i][curr.j]) continue;
+            if(curr.value > dist[curr.i][curr.j]) continue;
             for(int k=0; k<4; k++){
                 int nr = curr.i + dirs[k];
                 int nc = curr.j + dirs[k+1];
                 if(nr>=0 && nr<m && nc>=0 && nc<n){
-                    int curr_cost = Math.max(curr.value,Math.abs(heights[nr][nc]-heights[curr.i][curr.j]));
-                    if(curr_cost < cost[nr][nc]){
-                        cost[nr][nc] = curr_cost;
-                        pq.offer(new Pair(nr,nc,curr_cost));
+                    int d = Math.max(curr.value,Math.abs(heights[nr][nc] - heights[curr.i][curr.j]));
+                    if(d < dist[nr][nc]){
+                        dist[nr][nc] = d;
+                        pq.offer(new Pair(nr,nc,d));
                     }
                 }
             }
-          }
-          return cost[m-1][n-1];
-          
+        }
+        return dist[m-1][n-1];
     }
 }
