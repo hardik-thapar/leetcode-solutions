bc2fc07535f3d5b84e3fe72a430df0c437ed7505
Hardik Thapar <hardikthapar1@gmail.com>
2026-07-07 09:42:38 +0530

    Time: 20 ms (6.69%), Space: 47.4 MB (5.24%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
@@ -1,56 +1,62 @@
 class Solution {
     private class Pair{
         int node;
-        int cost;
+        int price;
         int stops;
-        Pair(int node, int cost, int stops){
+        Pair(int node, int price, int stops){
             this.node = node;
-            this.cost = cost;
+            this.price = price;
             this.stops = stops;
+        }}
+
+    private class Edge {
+        int node;
+        int price;
+        Edge(int node, int price){
+            this.node = node;
+            this.price = price;
         }
     }
+
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
-        List<Pair>[] adj = new ArrayList[n];
+        List<Edge>[] adj = new ArrayList[n];
         for(int i=0; i<n; i++){
             adj[i] = new ArrayList<>();
         }
-        for(int[] edge: flights){
-            int from = edge[0];
-            int to = edge[1];
-            int price = edge[2];
-            adj[from].add(new Pair(to,price,0));
+        for(int[] e: flights){
+            int from = e[0];
+            int to = e[1];
+            int price = e[2];
+            adj[from].add(new Edge(to,price));
         }
 
-        // dist[node][stops]
         int[][] dist = new int[n][k+2];
         for(int i=0; i<n; i++){
-            for(int j=0; j<k+2; j++){
-                dist[i][j] = Integer.MAX_VALUE;
-            }
+            Arrays.fill(dist[i],Integer.MAX_VALUE);
         }
 
-        // PQ(pair)
-        PriorityQueue<Pair> pq = new PriorityQueue<>((a,b)->Integer.compare(a.cost,b.cost));
+        PriorityQueue<Pair> pq = new PriorityQueue<>((a,b)->Integer.compare(a.price,b.price));
         dist[src][0] = 0;
         pq.offer(new Pair(src,0,0));
         while(!pq.isEmpty()){
             Pair curr = pq.poll();
-            if(curr.cost > dist[curr.node][curr.stops]) continue;
-            for(Pair nbr: adj[curr.node]){
-                int t_cost = curr.cost + nbr.cost;
-                int t_stops = curr.stops + 1;
+            if(curr.price > dist[curr.node][curr.stops]) continue;
+            for(Edge nbr: adj[curr.node]){
+                
+               int t_cost = curr.price + nbr.price;
+               int t_stops = curr.stops+1;
+
                 if(t_stops < k+2 && t_cost < dist[nbr.node][t_stops]){
                     dist[nbr.node][t_stops] = t_cost;
-                    pq.offer(new Pair(nbr.node,t_cost,t_stops));
-                }
+                    pq.offer(new Pair(nbr.node, t_cost, t_stops));
+                } 
             }
         }
         int res = Integer.MAX_VALUE;
         for(int i=0; i<k+2; i++){
             res = Math.min(res,dist[dst][i]);
         }
-        if(res == Integer.MAX_VALUE) return -1;
+        if(res==Integer.MAX_VALUE) return -1;
         return res;
-        
     }
 }
