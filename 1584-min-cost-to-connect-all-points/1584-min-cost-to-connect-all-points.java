850b5d5d48bbbcb54d16257500e40edca9581cb1
Hardik Thapar <hardikthapar1@gmail.com>
2026-07-08 09:48:28 +0530

    Time: 363 ms (38.96%), Space: 82.6 MB (41.8%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.java
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.java
@@ -1,9 +1,9 @@
 class Solution {
-    class Pair{
+    class Edge {
         int u;
         int v;
         int cost;
-        Pair(int u, int v, int cost){
+        Edge(int u, int v, int cost){
             this.u = u;
             this.v = v;
             this.cost = cost;
@@ -14,30 +14,30 @@ class Solution {
         return parent[node] = find(parent[node],parent);
     }
     public int minCostConnectPoints(int[][] points) {
-        int n = points.length;
-        List<Pair> edges = new ArrayList<>();
-        int x = 0;
-        int[] parent = new int[n+1];
-        for(int i=0; i<n+1; i++){
+        // whenver we have min cost/min price or same to connect all nodes -> we use kruskals algo (we sort the edges and use dsu(find and union)): we sort edges, then we start traversing edges from min cost to max cost, then start checking for an edge(u-v) if leaders of u and v are same then they belong to same group, so adding one more edge creates a cycle and no new node will be connected by that, so we dont union and skip, if leaders are diff that means they belong to diff groups(are not already connected) so we increment total_cost+=cost of that edge and then union them. 
+        List<Edge> edges = new ArrayList<>();
+        int[] parent = new int[points.length+1];
+        for(int i=0; i<points.length+1; i++){
             parent[i] = i;
         }
-        for(int i=0; i<n; i++){
-            for(int j=i+1; j<n; j++){
-                int cost = Math.abs(points[i][0]-points[j][0]) + Math.abs(points[i][1]-points[j][1]);
-                edges.add(new Pair(i,j,cost));
+        for(int i=0; i<points.length; i++){
+            for(int j=i; j<points.length; j++){
+                int cost = Math.abs(points[i][0]-points[j][0])+Math.abs(points[i][1] - points[j][1]);
+                edges.add(new Edge(i,j,cost));
             }
         }
         Collections.sort(edges, (a,b)->Integer.compare(a.cost,b.cost));
-        int ans = 0;
-        for(Pair edge: edges){
-            int p1 = find(edge.u, parent);
-            int p2 = find(edge.v, parent);
-            if(p1!=p2) {
+        int cost = 0;
+        for(Edge edge: edges){
+            int p1 = find(edge.u,parent);
+            int p2 = find(edge.v,parent);
+            if(p1!=p2){
                 parent[p2] = p1;
-                ans+=edge.cost;
+                cost+=edge.cost;
             }
         }
-        return ans;
 
+        return cost;
+        
     }
 }
