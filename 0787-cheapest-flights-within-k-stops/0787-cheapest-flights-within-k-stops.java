89479d1e27e998c688287e2d0550a44c7c2227c7
Hardik Thapar <hardikthapar1@gmail.com>
2026-07-14 10:38:18 +0530

    Time: 5 ms (98.94%), Space: 46.9 MB (17.05%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
@@ -1,62 +1,23 @@
 class Solution {
-    private class Pair{
-        int node;
-        int price;
-        int stops;
-        Pair(int node, int price, int stops){
-            this.node = node;
-            this.price = price;
-            this.stops = stops;
-        }}
-
-    private class Edge {
-        int node;
-        int price;
-        Edge(int node, int price){
-            this.node = node;
-            this.price = price;
-        }
-    }
-
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
-        List<Edge>[] adj = new ArrayList[n];
-        for(int i=0; i<n; i++){
-            adj[i] = new ArrayList<>();
-        }
-        for(int[] e: flights){
-            int from = e[0];
-            int to = e[1];
-            int price = e[2];
-            adj[from].add(new Edge(to,price));
-        }
+        int cost[] = new int[n];
+        Arrays.fill(cost,Integer.MAX_VALUE);
+        cost[src] = 0;
+        for(int i=0; i<k+1; i++){
+            int[] temp = cost.clone();
+            for(int[] edge: flights){
+                int u = edge[0];
+                int v = edge[1];
+                int w = edge[2];
 
-        int[][] dist = new int[n][k+2];
-        for(int i=0; i<n; i++){
-            Arrays.fill(dist[i],Integer.MAX_VALUE);
-        }
-
-        PriorityQueue<Pair> pq = new PriorityQueue<>((a,b)->Integer.compare(a.price,b.price));
-        dist[src][0] = 0;
-        pq.offer(new Pair(src,0,0));
-        while(!pq.isEmpty()){
-            Pair curr = pq.poll();
-            if(curr.price > dist[curr.node][curr.stops]) continue;
-            for(Edge nbr: adj[curr.node]){
-                
-               int t_cost = curr.price + nbr.price;
-               int t_stops = curr.stops+1;
-
-                if(t_stops < k+2 && t_cost < dist[nbr.node][t_stops]){
-                    dist[nbr.node][t_stops] = t_cost;
-                    pq.offer(new Pair(nbr.node, t_cost, t_stops));
-                } 
+                if(cost[u]!=Integer.MAX_VALUE && cost[u]+w < temp[v]){
+                    temp[v] = cost[u]+w;
+                }
             }
+            cost = temp; //we re init the cost to update it 
         }
-        int res = Integer.MAX_VALUE;
-        for(int i=0; i<k+2; i++){
-            res = Math.min(res,dist[dst][i]);
-        }
-        if(res==Integer.MAX_VALUE) return -1;
-        return res;
+
+        if(cost[dst]==Integer.MAX_VALUE) return -1;
+        return cost[dst];
     }
 }
