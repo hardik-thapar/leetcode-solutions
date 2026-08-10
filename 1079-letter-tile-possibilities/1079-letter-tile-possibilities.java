087d6ce5ddb1a3f4f930b3d6be10cc9bc662d6b5
Hardik Thapar <hardikthapar1@gmail.com>
2026-08-10 09:47:51 +0530

    Time: 3 ms (50.38%), Space: 42.4 MB (96.42%) - LeetHub

--- 1079-letter-tile-possibilities/1079-letter-tile-possibilities.java
+++ 1079-letter-tile-possibilities/1079-letter-tile-possibilities.java
@@ -1,21 +1,21 @@
 class Solution {
-    private int[] freq = new int[26];
-    // private List<String> res = new ArrayList<>();
-    int cnt=0;
+    int[] freq = new int[26];
+    int cnt = 0;
     private void dfs(){
         for(int i=0; i<26; i++){
             if(freq[i]>0){
-                freq[i]-=1;
                 cnt++;
+                freq[i]-=1;
                 dfs();
                 freq[i]+=1;
             }
         }
         return;
     }
+
     public int numTilePossibilities(String tiles) {
-        for(char c: tiles.toCharArray()){
-            freq[c-'A']++;
+        for(char x: tiles.toCharArray()){
+            freq[x-'A']++;
         }
         dfs();
         return cnt;
