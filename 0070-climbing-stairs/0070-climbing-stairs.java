d5a7261cec03097d5889cf901a38445437d782c0
Hardik Thapar <hardikthapar1@gmail.com>
2026-08-15 18:27:26 +0530

    Time: 0 ms (100%), Space: 42.2 MB (36.59%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,13 +1,13 @@
 class Solution {
-    private int dfs(int n, int[] steps){
-        if(n<=1) return 1;
-        if(steps[n]!=-1) return steps[n];
-        steps[n] = dfs(n-1, steps) + dfs(n-2, steps);
-        return steps[n];
+    private int dfs(int i, int[] dp){
+        if(i==0 || i==1) return 1;
+        if(dp[i]!=-1) return dp[i];
+        dp[i] = dfs(i-1, dp) + dfs(i-2, dp);
+        return dp[i];
     }
     public int climbStairs(int n) {
-        int[] steps = new int[n+1];
-        Arrays.fill(steps,-1);
-        return dfs(n, steps);
+        int[] dp = new int[n+1];
+        Arrays.fill(dp, -1);
+        return dfs(n, dp);
     }
 }
