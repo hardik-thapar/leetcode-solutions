fd3f5fb30011ab520ced5011a7911a5d416dc266
Hardik Thapar <hardikthapar1@gmail.com>
2026-08-18 14:53:20 +0530

    Time: 17 ms (89.68%), Space: 184 MB (24.6%) - LeetHub

--- 0403-frog-jump/0403-frog-jump.java
+++ 0403-frog-jump/0403-frog-jump.java
@@ -1,21 +1,16 @@
 class Solution {
     private Map<Integer, Integer> map = new HashMap<>();
-    private int helper(int pos, int[] stones){
-        // if(pos >= stones.length) return -1;
-        if(map.containsKey(pos)) return map.get(pos);
-        return -1;
-    }
     private boolean dfs(int idx, int k,Boolean[][] memo, int[] stones){
         // k-> jumps, idx-> index, memo-> dp arr
         if(stones[idx]==stones[stones.length-1]) return true;
         int pos = stones[idx];
         if(memo[idx][k]!=null) return memo[idx][k];
         boolean check = false;
-        if(k>1 && helper(pos+k-1, stones)!=-1) check = check || dfs(map.get(pos+k-1), k-1, memo, stones);
+        if(k>1 && map.get(pos+k-1)!=null) check = check || dfs(map.get(pos+k-1), k-1, memo, stones);
     
-        if(k>0 && helper(pos+k, stones)!=-1) check = check || dfs(map.get(pos+k), k, memo, stones);
+        if(k>0 && map.get(pos+k)!=null) check = check || dfs(map.get(pos+k), k, memo, stones);
 
-        if(helper(pos+k+1, stones)!=-1) check = check || dfs(map.get(pos+k+1), k+1, memo, stones);
+        if(map.get(pos+k+1)!=null) check = check || dfs(map.get(pos+k+1), k+1, memo, stones);
 
         memo[idx][k] = check;
         return memo[idx][k];
