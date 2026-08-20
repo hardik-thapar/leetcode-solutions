e6f0ed15e3c14bfcf8bc9ad01d5ed6ffeac5df6b
Hardik Thapar <hardikthapar1@gmail.com>
2026-08-20 09:34:10 +0530

    Time: 1 ms (79.97%), Space: 43.2 MB (33.72%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.java
+++ 0091-decode-ways/0091-decode-ways.java
@@ -1,25 +1,26 @@
 class Solution {
     private int cnt = 0;
+    private int dfs(int i, String s, int[] dp){
+        if(i>s.length()-1){
+           return 1;
+        }
 
-    private int dfs(int idx, String s, int[] dp){
-        if(idx==s.length()){
-            return 1;
+        if(dp[i]!=-1) return dp[i];
+        dp[i] = 0;
+        if(i <= s.length()-1){
+            int one = s.charAt(i) - '0'; 
+            if(one >= 1 && one <= 9) dp[i] += dfs(i+1, s, dp);
         }
-        if(dp[idx]!=-1) return dp[idx]+1;
-        if(idx+1 <= s.length()){
-            int one = s.charAt(idx) - '0';
-            if(one>=1 && one<=9) dp[idx]+= dfs(idx+1, s, dp);
+        if(i+1 <= s.length()-1){ 
+            int two = (s.charAt(i) - '0' )*10 + s.charAt(i+1)-'0';
+            if(two >= 10 && two <= 26) dp[i] += dfs(i+2, s, dp);
         }
-        if(idx+1 < s.length()){
-            int two = (s.charAt(idx) - '0')*10 + s.charAt(idx+1)-'0';
-            if(two>=10 && two<=26) dp[idx]+= dfs(idx+2, s, dp);
-        }
-        return dp[idx]+1;
+        return dp[i];
     }
     public int numDecodings(String s) {
-        int[] dp = new int[s.length()];
+        if(s.charAt(0)=='0') return 0;
+        int[] dp = new int[s.length()+1];
         Arrays.fill(dp, -1);
         return dfs(0, s, dp);
-
     }
 }
