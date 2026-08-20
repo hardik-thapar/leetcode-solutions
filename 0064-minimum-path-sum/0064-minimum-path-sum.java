ba4112bfe6c8b45bb615b5e8a7262a1394975b8a
Hardik Thapar <hardikthapar1@gmail.com>
2026-08-20 14:52:52 +0530

    Time: 1 ms (99.86%), Space: 50.4 MB (38.96%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -1,13 +1,14 @@
 class Solution {
     private int dfs(int r, int c, int[][] grid, int[][] memo){
-        if(r==grid.length-1 && c==grid[0].length-1) return grid[grid.length-1][grid[0].length-1];
+        if(r==grid.length-1 && c==grid[0].length-1) return grid[r][c];
         if(memo[r][c]!=-1) return memo[r][c];
-        memo[r][c] = 0;
-        int right = Integer.MAX_VALUE;
         int down = Integer.MAX_VALUE;
-        if(r+1 < grid.length) down = dfs(r+1, c, grid, memo);
-        if(c+1 < grid[0].length) right = dfs(r, c+1, grid, memo);
-        memo[r][c] = grid[r][c] + Math.min(right, down);
+        int right = Integer.MAX_VALUE;
+        
+        if(r+1 <= grid.length-1) down = dfs(r+1, c, grid, memo);
+        if(c+1 <= grid[0].length-1) right = dfs(r, c+1, grid, memo);
+        if(down==Integer.MAX_VALUE && right==Integer.MAX_VALUE) memo[r][c] = grid[r][c];
+        else memo[r][c] = grid[r][c] + Math.min(down, right);
         return memo[r][c];
     }
     public int minPathSum(int[][] grid) {
