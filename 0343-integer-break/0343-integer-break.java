76d31c1d5287ba80d3a060c829031b3645ac93bf
Hardik Thapar <hardikthapar1@gmail.com>
2026-08-20 09:42:34 +0530

    Time: 0 ms (100%), Space: 42 MB (74.25%) - LeetHub

--- 0343-integer-break/0343-integer-break.java
+++ 0343-integer-break/0343-integer-break.java
@@ -1,12 +1,11 @@
 class Solution {
-    private int dfs(int num, int[] dp){
+    private int dfs(int num, int dp[]){
         if(num==0) return 0;
         if(dp[num]!=-1) return dp[num];
-        int split = Integer.MIN_VALUE;
-        for(int i=1; i<=num/2; i++){
-            split = Math.max(split, (Math.max(i, dfs(i, dp)) * Math.max(num-i, dfs(num-i, dp))));
+        dp[num] = 0;
+        for(int i=1; i< num; i++){
+            dp[num] = Math.max(dp[num], Math.max(i, dfs(i, dp)) * Math.max(num-i, dfs(num-i, dp)));
         }
-        dp[num] = split;
         return dp[num];
     }
     public int integerBreak(int n) {
