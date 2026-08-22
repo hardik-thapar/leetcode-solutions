3dab430db5c12fbcd90fae1d674b37dbb615b928
Hardik Thapar <hardikthapar1@gmail.com>
2026-08-22 17:10:13 +0530

    Time: 39 ms (8.58%), Space: 47.2 MB (21.23%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change.java
@@ -0,0 +1,25 @@
+class Solution {
+    private int dfs(int idx, int target, int[] coins, int[][] memo){
+        if(idx==coins.length){
+            if(target==0) return 0;
+            return Integer.MAX_VALUE;
+        }
+        if(target==0) return 0;
+        if(target<0) return Integer.MAX_VALUE;
+        if(memo[idx][target]!=-1) return memo[idx][target];
+        int take = dfs(idx, target-coins[idx], coins, memo);
+        if(take!=Integer.MAX_VALUE) take +=1;
+        int skip = dfs(idx+1, target, coins, memo);
+        memo[idx][target] = Math.min(take, skip);
+        return memo[idx][target];
+    }
+    public int coinChange(int[] coins, int amount) {
+      int[][] memo = new int[coins.length][amount+1];
+      for(int i=0; i<coins.length; i++){
+        Arrays.fill(memo[i], -1);
+      }
+      int res = dfs(0, amount, coins, memo);
+      if(res!=Integer.MAX_VALUE) return res;
+      return -1;
+    }
+}
