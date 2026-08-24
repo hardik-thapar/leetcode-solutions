11e224d681e267f8ff71db9583f8bc8669873ae8
Hardik Thapar <hardikthapar1@gmail.com>
2026-08-24 20:47:14 +0530

    Time: 38 ms (9.42%), Space: 47 MB (31.21%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,25 +1,25 @@
 class Solution {
-    private int dfs(int idx, int target, int[] coins, int[][] memo){
+    private int dfs(int idx, int target, int[] coins, int[][] dp){
         if(idx==coins.length){
             if(target==0) return 0;
             return Integer.MAX_VALUE;
         }
         if(target==0) return 0;
         if(target<0) return Integer.MAX_VALUE;
-        if(memo[idx][target]!=-1) return memo[idx][target];
-        int take = dfs(idx, target-coins[idx], coins, memo);
-        if(take!=Integer.MAX_VALUE) take +=1;
-        int skip = dfs(idx+1, target, coins, memo);
-        memo[idx][target] = Math.min(take, skip);
-        return memo[idx][target];
+        if(dp[idx][target]!=-1) return dp[idx][target];
+        int take = dfs(idx, target-coins[idx], coins, dp);
+        if(take!=Integer.MAX_VALUE) take+=1;
+        int skip = dfs(idx+1, target, coins, dp);
+        dp[idx][target] = Math.min(take, skip);
+        return dp[idx][target];
     }
     public int coinChange(int[] coins, int amount) {
-      int[][] memo = new int[coins.length][amount+1];
-      for(int i=0; i<coins.length; i++){
-        Arrays.fill(memo[i], -1);
-      }
-      int res = dfs(0, amount, coins, memo);
-      if(res!=Integer.MAX_VALUE) return res;
-      return -1;
+        int[][] dp = new int[coins.length][amount+1];
+        for(int i=0; i<coins.length; i++){
+            Arrays.fill(dp[i], -1);
+        }
+        int res = dfs(0, amount, coins, dp);
+        if(res!=Integer.MAX_VALUE) return res;
+        return -1;
     }
 }
