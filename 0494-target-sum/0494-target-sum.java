5a157854401065cda28828982dfc70b37a9d5a21
Hardik Thapar <hardikthapar1@gmail.com>
2026-08-25 12:00:33 +0530

    Time: 10 ms (52.75%), Space: 47.2 MB (10.07%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,20 +1,18 @@
 class Solution {
     private int offset = 1000;
-    private int dfs(int idx, int target, int[] nums, int[][] dp){
+    private int dfs(int idx, int target, int[] nums, int[][] memo){
         if(idx==nums.length){
             if(target==0) return 1;
             return 0;
         }
         if(target<-1000 || target>1000) return 0;
-        if(dp[idx][target+offset]!=-1) return dp[idx][target+offset];
-        dp[idx][target+offset] = dfs(idx+1, target-nums[idx], nums, dp) + dfs(idx+1, target+nums[idx], nums, dp);
-        return dp[idx][target+offset];
+        if(memo[idx][target+offset]!=-1) return memo[idx][target+offset];
+        memo[idx][target+offset] = dfs(idx+1, target-nums[idx], nums, memo) + dfs(idx+1, target+nums[idx], nums, memo);
+        return memo[idx][target+offset];
     }
     public int findTargetSumWays(int[] nums, int target) {
-        int[][] dp = new int[nums.length][2001];
-        for(int i=0; i<nums.length; i++){
-            Arrays.fill(dp[i], -1);
-        }
-        return dfs(0, target, nums, dp);
+        int[][] memo = new int[nums.length][2001];
+        for(int i=0; i<nums.length; i++) Arrays.fill(memo[i], -1);
+        return dfs(0, target, nums, memo);
     }
 }
