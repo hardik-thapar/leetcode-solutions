edc49bbe6d1420a541bf2b5b0af6bcb18ce6e459
Hardik Thapar <hardikthapar1@gmail.com>
2026-08-25 14:20:11 +0530

    Time: 3 ms (56.37%), Space: 46.1 MB (7.79%) - LeetHub

--- 1049-last-stone-weight-ii/1049-last-stone-weight-ii.java
+++ 1049-last-stone-weight-ii/1049-last-stone-weight-ii.java
@@ -1,18 +1,17 @@
 class Solution {
-    private int offSet;
+    private int offset;
     private int dfs(int idx, int target, int[] stones, int[][] memo){
-        if(idx==stones.length){
-            return target;
-        }
-        if(memo[idx][target+offSet]!=-1) return memo[idx][target+offSet];
-        memo[idx][target+offSet] =  Math.min(Math.abs(dfs(idx+1, target-stones[idx], stones, memo)), Math.abs(dfs(idx+1, target+stones[idx], stones, memo)));
-        return memo[idx][target+offSet];
+        if(idx==stones.length) return target;
+        if(target<-offset || target>offset) return 0;
+        if(memo[idx][target+offset]!=-1) return memo[idx][target+offset];
+        memo[idx][target+offset] = Math.min(Math.abs(dfs(idx+1, target-stones[idx], stones, memo)),Math.abs(dfs(idx+1, target+stones[idx], stones, memo)));
+        return memo[idx][target+offset];
     }
     public int lastStoneWeightII(int[] stones) {
         int sum = 0;
         for(int i=0; i<stones.length; i++) sum+= stones[i];
-        offSet = sum;
-        int[][] memo = new int[stones.length][sum*2];
+        offset = sum;
+        int[][] memo = new int[stones.length][(sum*2)+1];
         for(int i=0; i<stones.length; i++) Arrays.fill(memo[i], -1);
         return dfs(1, stones[0], stones, memo);
     }
