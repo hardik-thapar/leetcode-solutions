553a40db159d323815973285ca63013c8f00f84d
Hardik Thapar <hardikthapar1@gmail.com>
2026-08-25 14:27:44 +0530

    Time: 146 ms (5.07%), Space: 260.8 MB (5%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,21 +1,21 @@
 class Solution {
-    // if we assume target as sum/2 -> and find any subset that sums to that then it means we can partition it
+    private int offset;
     private Boolean dfs(int idx, int target, int[] nums, Boolean[][] memo){
         if(idx==nums.length){
             if(target==0) return true;
             return false;
         }
-        if(target==0) return true;
-        if(target<0) return false;
-        if(memo[idx][target]!=null) return memo[idx][target];
-        memo[idx][target] = (dfs(idx+1, target-nums[idx], nums, memo)) || (dfs(idx+1, target, nums, memo));
-        return memo[idx][target];
+        if(memo[idx][target+offset]!=null) return memo[idx][target+offset];
+        memo[idx][target+offset] = (dfs(idx+1, target+nums[idx], nums, memo)) || (dfs(idx+1, target-nums[idx], nums, memo));
+        return memo[idx][target+offset];
     }
     public boolean canPartition(int[] nums) {
         int sum = 0;
-        for(int i=0; i<nums.length; i++) sum+=nums[i];
-        if(sum%2==1) return false;
-        Boolean[][] memo = new Boolean[nums.length][(sum/2)+1];
-        return dfs(0, sum/2, nums, memo);
+        for(int i=0; i<nums.length; i++){
+            sum+= nums[i];
+        }
+        offset = sum;
+        Boolean[][] memo = new Boolean[nums.length][(sum*2)+1];
+        return dfs(0, 0, nums, memo);
     }
 }
