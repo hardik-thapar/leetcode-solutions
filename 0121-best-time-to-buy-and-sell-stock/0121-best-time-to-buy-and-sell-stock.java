0a9db369368e18e108b1d87043f1a79e29d4610c
Hardik Thapar <hardikthapar1@gmail.com>
2026-08-29 19:31:42 +0530

    Time: 69 ms (1.45%), Space: 115.5 MB (15.53%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,12 +1,15 @@
 class Solution {
+    private int dfs(int i, int canBuy, int[] prices, int[][] memo){
+        // 1-> true, 0-> false
+        if(i==prices.length) return 0;
+        if(memo[i][canBuy]!=-1) return memo[i][canBuy];
+        if(canBuy==1) memo[i][canBuy] = Math.max(dfs(i+1, 0, prices, memo) - prices[i], dfs(i+1, 1, prices, memo));
+        else memo[i][canBuy] = Math.max(prices[i], dfs(i+1, 0, prices, memo)); 
+        return memo[i][canBuy];
+    }
     public int maxProfit(int[] prices) {
-        int m = prices[0];
-        int profit = 0;
-        for(int x: prices){
-            int curr = x - m;
-            profit = Math.max(profit, curr);
-            m = Math.min(m,x);
-        }
-        return profit;
+        int[][] memo = new int[prices.length][2];
+        for(int i=0; i<prices.length; i++) Arrays.fill(memo[i], -1);
+        return dfs(0, 1, prices, memo);
     }
 }
