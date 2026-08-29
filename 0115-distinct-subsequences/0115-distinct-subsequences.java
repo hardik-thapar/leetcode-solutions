6d182777fd86e939ae092ea51696dfea326cba30
Hardik Thapar <hardikthapar1@gmail.com>
2026-08-29 13:33:40 +0530

    Time: 17 ms (81.35%), Space: 54.2 MB (73.26%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -1,18 +1,18 @@
 class Solution {
-    private int dfs(int i, int j, char[] s, char[] t, int[][] memo){
-        if(i==s.length){
-            if(j==t.length) return 1;
+    private int dfs(int i, int j, char[] w1, char[] w2, int[][] memo){
+        if(i==w1.length){
+            if(j==w2.length) return 1;
             return 0;
         }
-        if(j==t.length) return 1;
+        if(j==w2.length) return 1;
         if(memo[i][j]!=-1) return memo[i][j];
         int take = 0;
-        if(s[i]==t[j]) take = dfs(i+1, j+1, s, t, memo);
-        take+= dfs(i+1, j, s, t, memo);
+        if(w1[i]==w2[j]) take+= dfs(i+1, j+1, w1, w2, memo);
+        take+= dfs(i+1, j, w1, w2, memo);
         return memo[i][j] = take;
     }
     public int numDistinct(String s, String t) {
-        int[][] memo = new int[s.length()][t.length()];
+        int[][] memo = new int[s.length()][t.length()+1];
         for(int i=0; i<s.length(); i++) Arrays.fill(memo[i], -1);
         return dfs(0, 0, s.toCharArray(), t.toCharArray(), memo);
     }
