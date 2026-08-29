a3a2bf714d99eef33dfca7a8ccff490e5c9f52df
Hardik Thapar <hardikthapar1@gmail.com>
2026-08-29 13:27:55 +0530

    Time: 3 ms (98.28%), Space: 47.2 MB (35.07%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -1,22 +1,16 @@
 class Solution {
-    private int dfs(int i, int j, char[] w1, char[] w2, int[][] memo){
-        if(i==w1.length && j==w2.length) return 0;
-        if(i==w1.length) return w2.length - j;
-        if(j==w2.length) return w1.length - i;
+    private int dfs(int i, int j, char[] s1, char[] s2, int[][] memo){
+        if(i==s1.length && j==s2.length) return 0;
+        if(i==s1.length) return s2.length - j;
+        if(j==s2.length) return s1.length - i;
         if(memo[i][j]!=-1) return memo[i][j];
-        /* 
-        if(equal) -> dfs(i+1, j+1)
-        replace -> 1 + dfs(i+1, j+1)
-        insert -> 1 + dfs(i, j+1)
-        deleting -> 1 + dfs(i+1, j)
-        */
-        if(w1[i]==w2[j]) memo[i][j] = dfs(i+1, j+1, w1, w2, memo);
-        else {
-            int replace = 1+dfs(i+1, j+1, w1, w2, memo);
-            int insert = 1+dfs(i, j+1, w1, w2, memo);
-            int delete = 1+dfs(i+1, j, w1, w2, memo);
-            memo[i][j] = Math.min(Math.min(insert, delete), replace);
-        }
+        // ways ->
+        // if equal-> then i+1,j+1
+        // if we replace-> 1 + i+1,j+1
+        // if insert -> 1 + i,j+1
+        // if delete-> 1+ i+1, j
+        if(s1[i]==s2[j]) memo[i][j] = dfs(i+1, j+1, s1, s2, memo);
+        else memo[i][j] = Math.min(Math.min(1+dfs(i+1,j+1, s1, s2, memo), 1+dfs(i+1,j, s1, s2, memo)), 1+dfs(i,j+1, s1, s2, memo));
         return memo[i][j];
     }
     public int minDistance(String word1, String word2) {
