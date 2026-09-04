8f3693a24e6398db1319f6b4152f29fc50f13185
Hardik Thapar <hardikthapar1@gmail.com>
2026-09-04 22:00:54 +0530

    Time: 36 ms (52.1%), Space: 46.5 MB (52.51%) - LeetHub

---
+++ 0935-knight-dialer/0935-knight-dialer.java
@@ -0,0 +1,35 @@
+class Solution {
+    private static final int[][] grid = {
+        {4,6},
+        {6,8},
+        {7,9},
+        {4,8},
+        {0,3,9},
+        {-1},
+        {0,1,7},
+        {2,6},
+        {1,3},
+        {2,4}
+    };
+    private static final int MOD = 1_000_000_007;
+    private long dfs(int digit, int n, long[][] dp){
+        if(n==0) return 1;
+        if(digit == 5) return 0;
+        if(dp[digit][n]!=-1) return dp[digit][n];
+        dp[digit][n] = 0;
+        for(int nbr: grid[digit]){
+            dp[digit][n] += dfs(nbr, n-1, dp);
+        }
+        return (dp[digit][n])%MOD;
+    }
+    public int knightDialer(int n) {
+        long[][] dp = new long[10][n+1];
+        for(int i=0; i<10; i++) Arrays.fill(dp[i], -1);
+        long res = 0;
+        for(int i=0; i<10; i++){
+            res += dfs(i, n-1, dp);
+        }
+        res = res%MOD;
+        return (int) res;
+    }
+}
